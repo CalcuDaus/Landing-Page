2fc35582428b28d106e7b9819e03fa6a38a61121
daus <dausg878@gmail.com>
2024-01-11 23:38:28 +0700

    adding header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content:["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        poppins : 'poppins'
+      },
+      colors : {
+        csBlue : '#2192FF',
+        csBlueOp : '#2192FF40',
+        csGreenOp : '#38E54D40',
+        csGreen : '#38E54D',
+        csGreenYellow : '#9CFF2E',
+        csYellow : '#FDFF00',
+      }
+    },
   },
   plugins: [],
 }
