26e278aef356d4db298f9ce4f3e9d1940d555401
daus <dausg878@gmail.com>
2024-01-13 23:51:34 +0700

    tes

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 document.querySelector('.menu').addEventListener('click',()=>{
     const dropdown = document.querySelector('.dropdown');
-    if(dropdown.classList.contains('h-[164px]')){
-        dropdown.classList.replace('h-[164px]','h-0');
+    if(dropdown.classList.contains('h-[214px]')){
+        dropdown.classList.replace('h-[214px]','h-0');
         dropdown.classList.replace('p-4','p-0');
     } else if(dropdown.classList.contains('h-0')){
-        dropdown.classList.replace('h-0','h-[164px]');
+        dropdown.classList.replace('h-0','h-[214px]');
         dropdown.classList.replace('p-0','p-4');
     }
 })
